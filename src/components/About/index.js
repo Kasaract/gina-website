0ac0c737ea270ce025c729627aebfb7617bd6bca
Kasaract <garynguyen2018@gmail.com>
2019-12-31 12:50:25 -0600

    Add structure for about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,18 @@ import React from 'react';
 import './about.scss';
 
 const About = () => {
-	return <div>About Page</div>;
+	return (
+		<div className="about-wrapper">
+			<div className="row">
+				<div className="img" />
+				<div className="text-wrapper">Hello world</div>
+			</div>
+			<div className="row">
+				<div className="text-wrapper">Hello world two</div>
+				<div className="img" />
+			</div>
+		</div>
+	);
 };
 
 export default About;
