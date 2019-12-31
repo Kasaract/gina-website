c830c55d3f8574b43210c6f04f91b6e3b6fb551a
Kasaract <garynguyen2018@gmail.com>
2019-12-31 17:29:46 -0600

    Add basic writing structure

--- src/components/Writing/index.js
+++ src/components/Writing/index.js
@@ -3,7 +3,69 @@ import React from 'react';
 import './writing.scss';
 
 const Writing = () => {
-	return <div>Writing Page</div>;
+	return (
+		<div className="writing-wrapper">
+			<div className="card">
+				<div className="card-img" />
+				<div className="card-title heading-text">What is Lorem Ipsum?</div>
+				<div className="card-text body-text">
+					Lorem Ipsum has been the industry's standard dummy text ever since the
+					1500s, when an unknown printer took a galley of type and scrambled it
+					to make a type specimen book...
+				</div>
+			</div>
+
+			<div className="card">
+				<div className="card-img" />
+				<div className="card-title heading-text">What is Lorem Ipsum?</div>
+				<div className="card-text body-text">
+					Lorem Ipsum has been the industry's standard dummy text ever since the
+					1500s, when an unknown printer took a galley of type and scrambled it
+					to make a type specimen book...
+				</div>
+			</div>
+
+			<div className="card">
+				<div className="card-img" />
+				<div className="card-title heading-text">What is Lorem Ipsum?</div>
+				<div className="card-text body-text">
+					Lorem Ipsum has been the industry's standard dummy text ever since the
+					1500s, when an unknown printer took a galley of type and scrambled it
+					to make a type specimen book...
+				</div>
+			</div>
+
+			<div className="card">
+				<div className="card-img" />
+				<div className="card-title heading-text">What is Lorem Ipsum?</div>
+				<div className="card-text body-text">
+					Lorem Ipsum has been the industry's standard dummy text ever since the
+					1500s, when an unknown printer took a galley of type and scrambled it
+					to make a type specimen book...
+				</div>
+			</div>
+
+			<div className="card">
+				<div className="card-img" />
+				<div className="card-title heading-text">What is Lorem Ipsum?</div>
+				<div className="card-text body-text">
+					Lorem Ipsum has been the industry's standard dummy text ever since the
+					1500s, when an unknown printer took a galley of type and scrambled it
+					to make a type specimen book...
+				</div>
+			</div>
+
+			<div className="card">
+				<div className="card-img" />
+				<div className="card-title heading-text">What is Lorem Ipsum?</div>
+				<div className="card-text body-text">
+					Lorem Ipsum has been the industry's standard dummy text ever since the
+					1500s, when an unknown printer took a galley of type and scrambled it
+					to make a type specimen book...
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Writing;
