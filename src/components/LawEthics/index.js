a6bfcf0e5dba62df530ae81a5d46c399d750074a
Kasaract <garynguyen2018@gmail.com>
2019-12-31 00:47:34 -0600

    Add file structure

---
+++ src/components/LawEthics/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './lawethics.scss';
+
+const LawEthics = () => {
+	return <div></div>;
+};
+
+export default LawEthics;
---
+++ src/share/NavBar/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './navbar.scss';
+
+const NavBar = () => {
+	return <div></div>;
+};
+
+export default NavBar;
