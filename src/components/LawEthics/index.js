60292080dc11f05f4f43bafdbd2cb5263979dc4d
Kasaract <garynguyen2018@gmail.com>
2019-12-31 17:54:35 -0600

    Add basic law and ethics structure

--- src/components/LawEthics/index.js
+++ src/components/LawEthics/index.js
@@ -3,7 +3,38 @@ import React from 'react';
 import './lawethics.scss';
 
 const LawEthics = () => {
-	return <div>Law {'&'} Ethics Page</div>;
+	return (
+		<div className="lawethics-wrapper">
+			<div className="left" />
+			<div className="right">
+				<div className="heading heading-text">Law {'&'} Ethics</div>
+				<div className="text body-text">
+					Lorem Ipsum has been the industry's standard dummy text ever since the
+					1500s, when an unknown printer took a galley of type and scrambled it
+					to make a type specimen book. Lorem Ipsum has been the industry's
+					standard dummy text ever since the 1500s, when an unknown printer took
+					a galley of type and scrambled it to make a type specimen book. Lorem
+					Ipsum has been the industry's standard dummy text ever since the
+					1500s, when an unknown printer took a galley of type and scrambled it
+					to make a type specimen book.
+				</div>
+				<div className="text body-text">
+					Lorem Ipsum has been the industry's standard dummy text ever since the
+					1500s, when an unknown printer took a galley of type and scrambled it
+					to make a type specimen book. Lorem Ipsum has been the industry's
+					standard dummy text ever since the 1500s, when an unknown printer took
+					a galley of type and scrambled it to make a type specimen book.
+				</div>
+				<div className="text body-text">
+					Lorem Ipsum has been the industry's standard dummy text ever since the
+					1500s, when an unknown printer took a galley of type and scrambled it
+					to make a type specimen book. Lorem Ipsum has been the industry's
+					standard dummy text ever since the 1500s, when an unknown printer took
+					a galley of type and scrambled it to make a type specimen book.
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default LawEthics;
