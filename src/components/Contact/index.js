50661326b70875d66327331067e149a665534aeb
Kasaract <garynguyen2018@gmail.com>
2019-12-31 18:10:35 -0600

    Add contact page structure

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,7 +3,18 @@ import React from 'react';
 import './contact.scss';
 
 const Contact = () => {
-	return <div>Contact Page</div>;
+	return (
+		<div className="contact-wrapper">
+			<div className="heading">Feel free to reach out!</div>
+			<ul className="social-media-list">
+				<li className="icon" />
+				<li className="icon" />
+				<li className="icon" />
+				<li className="icon" />
+				<li className="icon" />
+			</ul>
+		</div>
+	);
 };
 
 export default Contact;
