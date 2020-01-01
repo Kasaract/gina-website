f24501cd62acebc5732b2aaced1bda978a3abef4
Kasaract <garynguyen2018@gmail.com>
2019-12-31 18:56:19 -0600

    Add about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,10 +7,38 @@ const About = () => {
 		<div className="about-wrapper">
 			<div className="row">
 				<div className="img" />
-				<div className="text-wrapper">Hello world</div>
+				<div className="text-wrapper">
+					<div className="text">
+						Lorem Ipsum has been the industry's standard dummy text ever since
+						the 1500s, when an unknown printer took a galley of type and
+						scrambled it to make a type specimen book. Lorem Ipsum has been the
+						industry's standard dummy text ever since the 1500s, when an unknown
+						printer took a galley of type and scrambled it to make a type
+						specimen book. Lorem Ipsum has been the industry's standard dummy
+						text ever since the 1500s, when an unknown printer took a galley of
+						type and scrambled it to make a type specimen book.
+					</div>
+				</div>
 			</div>
 			<div className="row">
-				<div className="text-wrapper">Hello world two</div>
+				<div className="text-wrapper">
+					<div className="text">
+						Lorem Ipsum has been the industry's standard dummy text ever since
+						the 1500s, when an unknown printer took a galley of type and
+						scrambled it to make a type specimen book. Lorem Ipsum has been the
+						industry's standard dummy text ever since the 1500s, when an unknown
+						printer took a galley of type and scrambled it to make a type
+						specimen book.
+					</div>
+					<div className="text">
+						Lorem Ipsum has been the industry's standard dummy text ever since
+						the 1500s, when an unknown printer took a galley of type and
+						scrambled it to make a type specimen book. Lorem Ipsum has been the
+						industry's standard dummy text ever since the 1500s, when an unknown
+						printer took a galley of type and scrambled it to make a type
+						specimen book.
+					</div>
+				</div>
 				<div className="img" />
 			</div>
 		</div>
