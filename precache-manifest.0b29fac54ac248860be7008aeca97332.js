88517c808b871a553a7fd9ac744c1839dbff7c51
Kasaract <garynguyen2018@gmail.com>
2019-12-31 19:05:13 -0600

    Updates

---
+++ precache-manifest.0b29fac54ac248860be7008aeca97332.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d092ecb60ae04806e945ddf6343c8f0",
+    "url": "/gina-website/index.html"
+  },
+  {
+    "revision": "0bb3c4e7e71efb2b1c09",
+    "url": "/gina-website/static/css/main.1eab6ffb.chunk.css"
+  },
+  {
+    "revision": "6792de74b3b4733b08d9",
+    "url": "/gina-website/static/js/2.b4a1459f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/gina-website/static/js/2.b4a1459f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0bb3c4e7e71efb2b1c09",
+    "url": "/gina-website/static/js/main.ee90ad7c.chunk.js"
+  },
+  {
+    "revision": "434501365b5774b21173",
+    "url": "/gina-website/static/js/runtime-main.06531177.js"
+  }
+]);
