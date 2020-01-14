153c176a50cda1d51e1ef0736ba3e73c8024f3ba
Kasaract <garynguyen2018@gmail.com>
2020-01-14 17:48:10 -0500

    Adding logos to contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
 import './contact.scss';
+import Facebook from './images/facebook.svg';
+import Instagram from './images/instagram.svg';
+import LinkedIn from './images/linkedin.svg';
+import Twitter from './images/twitter.svg';
 
 const Contact = () => {
 	return (
 		<div className="contact-wrapper">
 			<div className="heading">Feel free to reach out!</div>
 			<ul className="social-media-list">
-				<li className="icon" />
-				<li className="icon" />
-				<li className="icon" />
-				<li className="icon" />
-				<li className="icon" />
+				<li className="icon-wrapper">
+					<img src={Facebook} className="icon" alt="social-media-icon" />
+				</li>
+				<li className="icon-wrapper">
+					<img src={Instagram} className="icon" alt="social-media-icon" />
+				</li>
+				<li className="icon-wrapper">
+					<img src={LinkedIn} className="icon" alt="social-media-icon" />
+				</li>
+				<li className="icon-wrapper">
+					<img src={Twitter} className="icon" alt="social-media-icon" />
+				</li>
 			</ul>
 		</div>
 	);
